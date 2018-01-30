691beb697eca07f436d0b9eeb784c3f837911faf
ahmed3ssem <a7med.assem@gmail.com>
2018-01-30 21:20:13 +0200

    sdsa

--- app/src/main/java/com/example/a7med/systemdemo/MainActivity.java
+++ app/src/main/java/com/example/a7med/systemdemo/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.a7med.systemdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt=findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
